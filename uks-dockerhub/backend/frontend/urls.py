58cbc22a2a9180dc26aa73a62c6e79fd93666011
0101dusica <trbovicdusica@gmail.com>
2026-02-06 13:41:10 +0100

    [M2-I7] implement login functionality with custom form and success page #7

--- uks-dockerhub/backend/frontend/urls.py
+++ uks-dockerhub/backend/frontend/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import registration_view, registration_success_view
+from .views import registration_view, registration_success_view, login_view, login_success_view
 
 urlpatterns = [
     path('', registration_view, name='register'),
     path('register/', registration_view, name='register'),
     path('register/success/', registration_success_view, name='registration-success'),
+    path('login/', login_view, name='login'),
+    path('login/success/', login_success_view, name='login-success'),
 ]
--- uks-dockerhub/backend/frontend/views.py
+++ uks-dockerhub/backend/frontend/views.py
@@ -1,5 +1,21 @@
 from django.shortcuts import render, redirect
-from users.forms import CustomUserCreationForm
+from users.forms import CustomUserCreationForm, CustomLoginForm
+from django.contrib.auth import login
+
+def login_view(request):
+    error = None
+    if request.method == 'POST':
+        form = CustomLoginForm(request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            if user is not None:
+                login(request, user)
+                return redirect('login-success')
+        else:
+            error = "Invalid username or password."
+    else:
+        form = CustomLoginForm()
+    return render(request, 'login.html', {'form': form, 'error': error})
 
 def registration_view(request):
     if request.method == 'POST':
@@ -13,3 +29,6 @@ def registration_view(request):
 
 def registration_success_view(request):
     return render(request, 'registration_success.html')
+
+def login_success_view(request):
+    return render(request, 'login_success.html')
